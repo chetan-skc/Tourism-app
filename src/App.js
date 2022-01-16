01c51f9bbb05b2789b8291f8b42692d7f4322ede
chetan-skc <goyalchetan118@gmail.com>
2022-01-16 21:53:07 +0530

    Day 1
    Just A Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Log in</h1>
+    <p>Email:</p>
+    <p>Password:</p>
+    <button id="btn">Login</button>
+    </div>
+  );
+}
+
+export default App;
