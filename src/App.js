1baf02a2e856b6a92555a0df6b1f4f6738cbd21b
chetan-skc <goyalchetan118@gmail.com>
2022-01-17 21:50:41 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Log in</h1>
-    <p>Email:</p>
-    <p>Password:</p>
-    <button id="btn">Login</button>
+      <h1>Log in</h1>
+      <p>Email:</p>
+      <p>Password:</p>
+      <button id="btn">Login</button>
+      <table>
+        <tr>
+          <th>Name</th>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>Elsa</td>
+        </tr>
+      </table>
     </div>
   );
 }
